38f144558a1f9b0b252702ebac8ef116bd577a40
Elainne Silva <1222629@isep.ipp.pt>
2024-03-20 23:23:12 +0000

    [calendar-elainne]

---
+++ aula1-calendar-elainne.js
@@ -0,0 +1,23 @@
+let diasDaSemana = "TERÇA";
+let mes = 31;
+
+console.log("D    S    T    Q    Q    S    S");
+console.log("-------------------------------");
+console.log("          1    2    3    4    5")
+
+
+console.log("D    S    T    Q    Q    S    S");
+console.log("-------------------------------");
+console.log("                1    2    3   4")
+
+console.log("D    S    T    Q    Q    S    S");
+console.log("-------------------------------");
+console.log("                    1    2    3")
+
+console.log("D    S    T    Q    Q    S    S");
+console.log("-------------------------------");
+console.log("                         1    2")
+
+console.log("D    S    T    Q    Q    S    S");
+console.log("-------------------------------");
+console.log("                              1\n2")
