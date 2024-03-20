ca1bd84f565f33a0f6d237a25cc42753df94b340
Antonio <rodrigowserra@gmail.com>
2024-03-20 22:58:59 +0000

    calendar

---
+++ aula1-calendar.js
@@ -0,0 +1,11 @@
+let diasDaSemana = "SEGUNDA";
+let mes = 31;
+
+console.log("D    S    T    Q    Q    S    S");
+console.log("-------------------------------");
+console.log("               1    2    3    4\n5")
+
+
+console.log("D    S    T    Q    Q    S    S");
+console.log("-------------------------------");
+console.log("                    1    2    3")
