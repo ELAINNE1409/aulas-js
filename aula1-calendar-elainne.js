935aefa9213b800fabd0f5cfb27ee316ae96f432
Antonio <rodrigowserra@gmail.com>
2024-03-27 21:09:30 +0000

    aula2

--- aula1-calendar-elainne.js
+++ aula1-calendar-elainne.js
@@ -3,21 +3,21 @@ let mes = 31;
 
 console.log("D    S    T    Q    Q    S    S");
 console.log("-------------------------------");
-console.log("          1    2    3    4    5")
+console.log("          1    2    3    4    5");
 
 
 console.log("D    S    T    Q    Q    S    S");
 console.log("-------------------------------");
-console.log("                1    2    3   4")
+console.log("                1    2    3   4");
 
 console.log("D    S    T    Q    Q    S    S");
 console.log("-------------------------------");
-console.log("                    1    2    3")
+console.log("                    1    2    3");
 
 console.log("D    S    T    Q    Q    S    S");
 console.log("-------------------------------");
-console.log("                         1    2")
+console.log("                         1    2");
 
 console.log("D    S    T    Q    Q    S    S");
 console.log("-------------------------------");
-console.log("                              1\n2")
+console.log("                              1\n2");
